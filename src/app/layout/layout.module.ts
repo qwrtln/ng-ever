8245ef3ca2742f6d0dbe32c4b8d57bcf1ee9e4b5
Szewa <marcin91.szewczyk@gmail.com>
2018-11-17 09:38:30 +0100

    Add Layout module

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {LayoutComponent} from './layout.component';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  declarations: [LayoutComponent],
+  imports: [
+    CommonModule
+    ],
+  exports: [
+    LayoutComponent
+  ]
+})
+export class LayoutModule {
+
+}
